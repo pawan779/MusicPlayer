13f66419fa44ddb90ffc68176d99396f9d95b20a
Pawan Dharel <38077601+pawan779@users.noreply.github.com>
2024-07-04 20:29:25 -0400

    recommendation query

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,15 +1,49 @@
-import { FlatList, StyleSheet } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet } from "react-native";
 import { Text, View } from "@/src/components/Themed";
-import { tracks } from "@/assets/data/tracks";
 import TrackListItem from "@/src/components/TrackListItem";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            height
+            url
+            width
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: "pop" },
+  });
+
+  if (loading) return <ActivityIndicator />;
+  if (error) return <Text>Error! {error.message}</Text>;
+
+  const tracks = data?.recommendations?.tracks || [];
   return (
     <View style={styles.container}>
       <FlatList
         data={tracks}
         showsVerticalScrollIndicator={false}
         keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{ paddingBottom: 70 }}
         renderItem={({ item }) => <TrackListItem track={item} />}
       />
     </View>
